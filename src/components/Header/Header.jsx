b9401217dd30e6ea716a9b378871392f94a28fba
ivanpalachuk <ivanzar@hotmail.com>
2023-03-24 10:12:23 -0300

    [FIX] Navbar on mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/esm/Button';
 import { useAuth } from '../../context/AuthContext';
 
-
 const Header = () => {
-
   const { user, logOut } = useAuth()
 
   const handleClick = () => { logOut() }
-  
 
   return (
     <header style={{ marginBottom: "5rem" }}>
-      <Navbar bg="dark" variant="dark" fixed='top'>
+      <Navbar bg="dark" variant="dark" expand="md" fixed='top'>
         <Container>
           <Navbar.Brand href="/">
             <img
@@ -26,22 +23,27 @@ const Header = () => {
               alt="React Bootstrap logo"
             />
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/novedades">Novedades</Nav.Link>
-            <Nav.Link href="/toprated">Recomendadas</Nav.Link>
-            <Nav.Link href="/favourites">Favoritos</Nav.Link>
-          </Nav>
-          {user ?
-            <>
-              <Search />
-              <Button variant="danger" type="submit" href='/login' style={{ marginLeft: '10px' }} onClick={handleClick}>Logout</Button>
-            </> :
-            <>
-              <Button variant="success" type="submit" href='/login' style={{ marginRight: '10px' }}>Ingresa</Button>
-              <Button variant="outline-secondary" type="submit" href='/register'>Registrate</Button>
-            </>
-          }
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/novedades">Novedades</Nav.Link>
+              <Nav.Link href="/toprated">Recomendadas</Nav.Link>
+              <Nav.Link href="/favourites">Favoritos</Nav.Link>
+            </Nav>
+            <Nav className="ms-auto">
+              {user ?
+                <>
+                  <Search />
+                  <Button variant="danger" type="submit" href='/login' style={{ marginLeft: '10px' }} onClick={handleClick}>Logout</Button>
+                </> :
+                <>
+                  <Button variant="success" type="submit" href='/login' style={{ marginRight: '10px' }}>Ingresa</Button>
+                  <Button variant="outline-secondary" type="submit" href='/register'>Registrate</Button>
+                </>
+              }
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
